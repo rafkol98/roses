a7e55bc76b923119aec4c7acaa9c2b76b085eabb
rafaelkoll <rafcall98@hotmail.com>
2020-11-04 13:48:15 +0000

    input working

--- js/validation.js
+++ js/validation.js
@@ -1,10 +1,29 @@
    $("#addBtn").click(function() {
-    const requestBricks = firebase.functions().httpsCallable('writeName');
-    requestBricks({name: 'Alex', threeWords:"NICE, FUNNY, PROFESSOR"}).then(result => {
-        console.log(result.data);
-        alert(result.data);
-        // window.location.href = "loggedIn.html";
-    });
+
+    var name =  $("#name").val();
+    var threeWords =  $("#threeWords").val();
+
+  
+    if(name.trim().indexOf(' ') == -1 && name.length <25){ 
+        console.log(name +" " +threeWords);
+        const requestBricks = firebase.functions().httpsCallable('writeName');
+        requestBricks({name: name, threeWords:threeWords}).then(result => {
+            console.log(result.data);
+            alert(result.data);
+            // window.location.href = "loggedIn.html";
+        });
+
+
+
+
+    } else {
+        alert("Please enter just the first name (1 word) - max characters allowed are 25.");
+    }
+
+
+
+
+    
         
     });
     
